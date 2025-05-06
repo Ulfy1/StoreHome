e6eced12a4875207ed18427e1489a80ec5325f3c
Ulfy <yaroslav.zix@gmail.com>
2025-05-06 18:39:33 +0200

    Login form & controller ready

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
+class User(AbstractUser):
+    image =  models.ImageField(upload_to='users_images', null=True, blank=True, verbose_name='profile picture')
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.username
--- app/users/views.py
+++ app/users/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from users.forms import UserLoginForm
+from django.contrib import auth
+from django.urls import reverse
 
 # Create your views here.
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+
     context = {
-        'title': 'Home - Login'
+        'title': 'Home - Login',
+        'form': form,
     }
     return render(request, 'users/login.html', context)
 
